31ef567cead6653bc9e3d349c6fe7184eafa9b6c
Sumanth Reddy <dsumanthreddy6@gmail.com>
2024-09-15 15:00:16 -0400

    Added server, Axios and ChartJS

--- server.js
+++ server.js
@@ -1,34 +1,29 @@
-// Budget API
-
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
 const app = express();
 const port = 3000;
-
-app.use(cors());
-
+app.use("/", express.static("public"));
 const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
+  myBudget: [
+    {
+      title: "Eat out",
+      budget: 25,
+    },
+    {
+      title: "Rent",
+      budget: 275,
+    },
+    {
+      title: "Grocery",
+      budget: 110,
+    },
+  ],
 };
-
-
-app.get('/budget', (req, res) => {
-    res.json(budget);
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
 });
-
+app.get("/budget", (req, res) => {
+  res.json(budget);
+});
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
