07b4ba7ae16d99eea409bf6c0c021618dafccd84
Sumanth Reddy <dsumanthreddy6@gmail.com>
2024-10-13 20:22:11 -0400

    fetching data from mongo

--- server.js
+++ server.js
@@ -3,14 +3,59 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 app.use("/", express.static("public"));
-const budget = require("./budgetData.json");
+app.use(express.json());
+
+const mongoose = require("mongoose");
+const budgetModel = require("./models/budget_schema");
+
+let url = "mongodb://localhost:27017/budgetdatabase";
 app.use(cors());
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
+
 app.get("/budget", (req, res) => {
-  res.json(budget);
+  mongoose
+    .connect(url)
+    .then(() => {
+      console.log("Connected to database");
+      // Fetch
+      budgetModel
+        .find({})
+        .then((data) => {
+          res.send(data);
+          mongoose.connection.close();
+        })
+        .catch((connectionError) => {
+          console.log(connectionError);
+        });
+    })
+    .catch((connectionError) => {
+      console.log(connectionError);
+    });
 });
+
+app.post("/addBudgetData", (req, res) => {
+  mongoose
+    .connect(url)
+    .then(() => {
+      // Insert
+      let newData = new budgetModel(req.body);
+      budgetModel
+        .insertMany(newData)
+        .then((data) => {
+          res.send("Data Entered Successfully");
+          mongoose.connection.close();
+        })
+        .catch((connectionError) => {
+          res.send(connectionError.message);
+        });
+    })
+    .catch((connectionError) => {
+      res.send(connectionError);
+    });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
